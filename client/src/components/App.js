1f0a3ecfa0c87c24d1d85f82dbf614699aae4adf
Clayton <claytonajones839@gmail.com>
2019-08-30 18:30:16 -0400

    Add Routes and componenets for login and register

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import TacoIndex from './tacos/TacoIndex';
+import Login from "./Login";
+import Register from "./Register";
 import { Route, HashRouter, Switch } from 'react-router-dom';
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
       <h1>UNSHELLED</h1>
       <HashRouter>
         <Route exact path="/" component={TacoIndex} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </HashRouter>
     </div>
   );
