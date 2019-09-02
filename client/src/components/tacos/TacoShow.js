372bc116f4ea978bea69de58eabf4525e1c4d5c3
reynier25 <fsoto@uchicago.edu>
2019-09-02 11:17:05 -0700

    taco show styling

--- client/src/components/tacos/TacoShow.js
+++ client/src/components/tacos/TacoShow.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import Queries from "../../graphql/queries";
-
+import "../../TacoShow.scss";
+import { Link } from "react-router-dom";
 
 class TacoShow extends Component {
 
@@ -20,16 +21,55 @@ class TacoShow extends Component {
                 // debugger;
                 return (
                   <div className="detail">
-                    hello im a taco
-                    <div>{data.taco._id}</div>
-                    <div>{data.taco.name}</div>
-                    <div>{data.taco.description}</div>
-                    <div>{data.taco.style}</div>
-                    <div>{data.taco.rating}</div>
-                    <div>{data.taco.price}</div>
-                    <div>{data.taco.restaurant._id}</div>
-                    <div>{data.taco.restaurant.name}</div>
-                    <div>{data.taco.restaurant.location}</div>
+                    <div className="center-boxes">
+                      <div className="taco-info-box">
+                        {/* <div>{data.taco._id}</div> */}
+                        <div className="taco-name">{data.taco.name}</div>
+                        <div className="taco-description">
+                          {data.taco.description}
+                        </div>
+                        <div className="taco-style">{data.taco.style}</div>
+                        <div className="taco-rating">{data.taco.rating}</div>
+                        <div className="taco-price">{data.taco.price}</div>
+                        {/* <div>{data.taco.restaurant._id}</div> */}
+                        <div className="restaurant-name">
+                          {data.taco.restaurant.name}
+                        </div>
+                        <div className="restaurant-location">
+                          {data.taco.restaurant.location}
+                        </div>
+                      </div>
+
+                      <div className="taco-pics">taco pics</div>
+
+                      <div className="taco-activity-box">
+                        <div className="header">Global Recent Activity</div>
+                        <div className="activity-box">
+                          activity box index goes here
+                        </div>
+                        <div className="footer">
+                          <button className="show-more">Show More</button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="right-boxes">
+                      <div className="proposal-box">
+                        <Link to={`/taco/propose_edit/${data.taco._id}`}>Propose Edit</Link>
+                        <Link to={`/taco/propose_dupe/${data.taco._id}`}>Propose Duplicate</Link>
+                      </div>
+                      <div className="taco-loyalists-box">taco loyalists</div>
+
+                      <div className="similar-tacos-box">similar tacos</div>
+
+                      <div className="nearby-locations-box">
+                        nearby locations
+                      </div>
+
+                      <div className="popular-locations-box">
+                        popular locations
+                      </div>
+                    </div>
                   </div>
                 );
             }}
