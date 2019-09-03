091757733d482b59d9feb6f5b74fcded2121df3f
reynier25 <fsoto@uchicago.edu>
2019-09-02 17:03:19 -0700

    taco styling cont

--- client/src/components/tacos/TacoShow.js
+++ client/src/components/tacos/TacoShow.js
@@ -24,20 +24,30 @@ class TacoShow extends Component {
                     <div className="center-boxes">
                       <div className="taco-info-box">
                         {/* <div>{data.taco._id}</div> */}
-                        <div className="taco-name">{data.taco.name}</div>
-                        <div className="taco-description">
-                          {data.taco.description}
+
+                        <div className="header">
+                          <div className="logo-box">Im logo</div>
+                          <div className="taco-info">
+                            <div className="taco-name">{data.taco.name}</div>
+                            <div className="restaurant-name">{data.taco.restaurant.name}</div>
+                            {/* <div className="taco-description">{data.taco.description}</div> */}
+                            <div className="taco-style">{data.taco.style}</div>
+                            <div className="taco-rating">{data.taco.rating}</div>
+                            {/* <div className="taco-price">{data.taco.price}</div> */}
+                            {/* <div>{data.taco.restaurant._id}</div> */}
+                            {/* <div className="restaurant-location">{data.taco.restaurant.location}</div> */}
+                          </div>
+                          <div className="taco-check-ins">
+                              <div className="total"></div>
+                              <div className="unique"></div>
+                              <div className="monthly"></div>
+                              <div className="you"></div>
+                          </div>
                         </div>
-                        <div className="taco-style">{data.taco.style}</div>
-                        <div className="taco-rating">{data.taco.rating}</div>
-                        <div className="taco-price">{data.taco.price}</div>
-                        {/* <div>{data.taco.restaurant._id}</div> */}
-                        <div className="restaurant-name">
-                          {data.taco.restaurant.name}
-                        </div>
-                        <div className="restaurant-location">
-                          {data.taco.restaurant.location}
-                        </div>
+                        
+                        <div className="info-bar">Info bar</div>
+                        
+                        <div className="show-more">Show more</div>
                       </div>
 
                       <div className="taco-pics">taco pics</div>
@@ -55,19 +65,43 @@ class TacoShow extends Component {
 
                     <div className="right-boxes">
                       <div className="proposal-box">
-                        <Link to={`/taco/propose_edit/${data.taco._id}`}>Propose Edit</Link>
-                        <Link to={`/taco/propose_dupe/${data.taco._id}`}>Propose Duplicate</Link>
+                        <Link to={`/taco/propose_edit/${data.taco._id}`}>
+                          Propose Edit
+                        </Link>
+                        <Link to={`/taco/propose_dupe/${data.taco._id}`}>
+                          Propose Duplicate
+                        </Link>
                       </div>
-                      <div className="taco-loyalists-box">taco loyalists</div>
+                      <div className="taco-loyalists-box">
+                        <div className="header">Loyal Tacoists</div>
+                        <div className="tacoists-index">
+                          <div className="tacoist">Tacoist x12</div>
+                        </div>
+                      </div>
 
-                      <div className="similar-tacos-box">similar tacos</div>
+                      <div className="similar-tacos-box">
+                        <div className="header">Similar Tacos</div>
+                        <div className="similar-tacos-index">
+                          <div className="similar-taco">A similar taco x5</div>
+                        </div>
+                      </div>
 
                       <div className="nearby-locations-box">
-                        nearby locations
+                        <div className="header">Nearby Verified Locations</div>
+                        <div className="nearby-locations-index">
+                          <div className="nearby-location">
+                            Nearby Location x3
+                          </div>
+                        </div>
                       </div>
 
                       <div className="popular-locations-box">
-                        popular locations
+                        <div className="header">Popular Locations</div>
+                        <div className="popular-locations-index">
+                          <div className="popular-location">
+                            Popular Location x10
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
