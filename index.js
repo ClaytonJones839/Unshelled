adfad56bdfc3532d04f7b800c1cf450008835fdd
Clayton <claytonajones839@gmail.com>
2019-09-06 14:13:15 -0400

    Change more files

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = require("./server/server");
 
 const port = process.env.PORT || 5000;
 const path = require('path');
+
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('client/build'));
     app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
